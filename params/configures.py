eebaf9d27a235112ef5f4d27680392d29628c127
Junru Xiong <junruxiong@gmail.com>
2021-12-21 02:00:14 +0000

    2112

---
+++ params/configures.py
@@ -0,0 +1,52 @@
+import os
+
+# read_file paths
+class Config_path:
+    parent_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    current_path = os.path.join(parent_path, 'data')  # "data" is current folder
+
+    wiki_train_path = os.path.join(current_path, 'annotated_wd_data_train.txt')
+    wiki_dev_path = os.path.join(current_path, 'dev_combination_wiki.csv')  ###############
+    wiki_test_path = os.path.join(current_path, 'annotated_wd_data_test.txt')
+
+    train_raw_path = os.path.join(current_path, 'train_raw.csv')  ###############
+    test_raw_path = os.path.join(current_path, 'test_raw.csv')  ###############
+
+    train_q_space_path = os.path.join(current_path, 'train_q_space.csv')  ###################
+    train_combination_path = os.path.join(current_path, 'train_combination.csv')
+    test_combination_path = os.path.join(current_path, 'test_combination.csv')
+
+    train_web = os.path.join(current_path, 'WebQSP/data/WebQSP.train.json')  ###################
+    test_web = os.path.join(current_path, 'WebQSP/data/WebQSP.test.json')  ###################
+
+    triple = os.path.join(current_path, 'wiki_triple_small.csv')
+
+    mapping_path = os.path.join(current_path, 'mapping.txt')
+    checkpoint_path = "checkpoint"
+
+
+# output paths
+class Config_output_path:
+    parent_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    current_path = os.path.join(parent_path, 'multiHop_QA/models')
+
+    ner_data_path = os.path.join(parent_path, 'data')
+    transformers_path = os.path.join(parent_path, 'multiHop_QA/models')
+
+
+# parameters for Seq2Seq transformer model
+class Hyparams_transformers:
+    BATCH_SIZE = 128
+    HID_DIM = 256
+    ENC_LAYERS = 3
+    DEC_LAYERS = 3
+    ENC_HEADS = 8
+    DEC_HEADS = 8
+    ENC_PF_DIM = 512
+    DEC_PF_DIM = 512
+    ENC_DROPOUT = 0.1
+    DEC_DROPOUT = 0.1
+    LEARNING_RATE = 0.0001
+
+    N_EPOCHS = 5000
+    CLIP = 1
